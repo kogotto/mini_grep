d1faf26a19388ab3f464e9eb3bc37a1e85b6b865
kogotto <kogotto@gmail.com>
2026-08-01 03:06:53 +0300

    Add filter by characters and *

--- CLogReader.cpp
+++ CLogReader.cpp
@@ -1,11 +1,75 @@
 #include "CLogReader.hpp"
 
-#include <cctype>
+#include <vector>
 
 namespace {
 
+using Row = std::vector<bool>;
+using Matrix = std::vector<Row>;
+
+Row emptyRow(size_t size) {
+	return Row(size, false);
+}
+
+Matrix emptyMatrix(size_t rows, size_t cols) {
+	Matrix result;
+	result.reserve(rows);
+	for (int i = 0; i < rows; ++i) {
+		result.emplace_back(emptyRow(cols));
+	}
+	return result;
+}
+
 bool match(const char* str, const char* filter) {
-	return std::isalpha(str[0]);
+	const auto strSize = strlen(str);
+	const auto filterSize = strlen(filter);
+	auto matrix = emptyMatrix(strSize + 1, filterSize + 1);
+
+	matrix[0][0] = true;
+	for (int i = 1; i <= strSize; ++i) {
+		matrix[i][0] = false;
+	}
+	for (int i = 0; i <= strSize; ++i) {
+		for (int j = 1; j <= filterSize; ++j) {
+			switch (filter[j - 1]) {
+			case '*':
+				if (i == 0) {
+					matrix[i][j] = matrix[i][j - 1];
+				}
+				else {
+					matrix[i][j] = matrix[i - 1][j] || matrix[i][j - 1];
+				}
+				break;
+			default:
+				if (i == 0) {
+					matrix[i][j] = false;
+				}
+				else {
+					matrix[i][j] = matrix[i - 1][j - 1] && filter[j - 1] == str[i - 1];
+				}
+				break;
+			}
+		}
+	}
+
+	return matrix[strSize][filterSize];
+}
+
+bool readLine(char* buf, int bufSize, FILE* file) {
+	if (feof(file)) {
+		return false;
+	}
+
+	int nextPlace = 0;
+	for (; nextPlace < bufSize - 1;) {
+		const int c = fgetc(file);
+		if (c == EOF || c == '\n') {
+			break;
+		}
+		buf[nextPlace++] = static_cast<char>(c);
+	}
+	buf[nextPlace] = '\0';
+	return true;
 }
 
 } // namespace
@@ -29,7 +93,7 @@ bool CLogReader::SetFilter(const char* filter) {
 }
 
 bool CLogReader::GetNextLine(char* buf, const int bufSize) {
-	while (fgets(buf, bufSize, file_) != nullptr) {
+	while (readLine(buf, bufSize, file_)) {
 		if (match(buf, filter_)) {
 			return true;
 		}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@
 
 constexpr int BUF_SIZE = 1024;
 
+bool filterFile(const char* filename, const char* filter) {
+	CLogReader reader;
+	if (!reader.Open(filename)) {
+		std::cerr << "Can't open file \"" << filename << "\"\n";
+		return false;
+	}
+
+	if (!reader.SetFilter(filter)) {
+		std::cerr << "Can't set filter \"" << filter << "\"\n";
+		return false;
+	}
+
+	std::cout << ":::: filter \"" << filter << "\"\n";
+	char buf[BUF_SIZE];
+	while (reader.GetNextLine(buf, BUF_SIZE)) {
+		std::cout << buf << "\n";
+	}
+	return true;
+}
+
+#if 0
 int main()
 {
 	CLogReader reader;
@@ -13,9 +34,27 @@ int main()
 		return 1;
 	}
 
+	const char* const filter = "*";
+	if (!reader.SetFilter(filter)) {
+		std::cerr << "Can't set filter \"" << filter << "\"\n";
+		return false;
+	}
+
 	char buf[BUF_SIZE];
 	while (reader.GetNextLine(buf, BUF_SIZE)) {
 		std::cout << buf;
 	}
 	return 0;
 }
+#else
+int main() {
+	filterFile("input", "*");
+	filterFile("input", "qwer");
+	filterFile("input", "q*");
+	filterFile("input", "*r");
+	filterFile("input", "q*r");
+	filterFile("input", "qw*r");
+	filterFile("input", "q*er");
+	filterFile("input", "*r*");
+}
+#endif
